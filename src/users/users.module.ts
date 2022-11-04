d5cf0c81c1617fd59e2f91e8b53aa3879fdd6012
Savinykh Vladimir <awpm9so@gmail.com>
2022-11-04 12:53:21 +0300

    добавил авторизацию\регистрацию

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -10,6 +10,7 @@ import { UsersService } from './users.service';
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([User, Role, UserRoles]), RolesModule]
+  imports: [SequelizeModule.forFeature([User, Role, UserRoles]), RolesModule],
+  exports: [UsersService]
 })
 export class UsersModule {}
